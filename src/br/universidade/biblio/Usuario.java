f1df13aed82727c15e57b7dc7a656fed6d13f99d
jamilly-campelo <97696411+jamilly-campelo@users.noreply.github.com>
2025-09-04 13:44:57 -0300

    feat: creating Usuario class and Main.java

---
+++ src/br/universidade/biblio/Usuario.java
@@ -0,0 +1,18 @@
+package br.universidade.biblio;
+
+public class Usuario {
+	private String nome;
+	private String matricula;
+	private String curso;
+	
+	// Construtor
+	public Usuario(String nome, String matricula, String curso) {
+		this.nome = nome;
+		this.matricula = matricula;
+		this.curso = curso;
+	}
+	
+	public void getInfo() {
+		
+	}
+}
